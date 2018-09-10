86b0093d7ac289d751e33318b9ea9b5956a3e7b3
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 17:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function currentLine (line) {
   for (let i = 0; i < line.length; i++) {
     str = str + `${i + 1}. ${line[i]}, `;
   }
-  str.substr(0,str.length-2)
+  str.substring(0,str.length-2)
   return str;
 }
