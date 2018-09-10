f77e71a239ecce78a964b7105229505e6a85f5d2
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 17:01:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber (currentLine, name) {
 
-  return `Welcome, ${name}. You are number ${currentLine} in line`
+  return `Welcome, ${name}. You are number ${currentLine} in line.`
 }
