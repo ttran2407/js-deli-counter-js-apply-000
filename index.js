8d8a5e214bb12beb5df032d06603e7521d2834c1
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 17:58:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ function takeANumber (currentLine, name) {
 }
 
 function nowServing (currentLine) {
+  //return string "..." if line length is 0
   if (currentLine.length === 0) {
 
     return "There is nobody waiting to be served!"
   }
+  //removing first customer name of the line and then call his/her name
   var firstName = currentLine.shift();
 
   return `Currently serving ${firstName}.`
@@ -20,11 +22,11 @@ function currentLine (line) {
     return "The line is currently empty.";
   }
   var str = "The line is currently: ";
-
-
+//loop through line to add customer's names and their current line to the str
   for (let i = 0; i < line.length; i++) {
     str = str + `${i + 1}. ${line[i]}, `;
   }
+  //removing to last character in the str ", "
   return str.substring(0, str.length - 2);
 
 }
